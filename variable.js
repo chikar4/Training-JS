5557c94542e9eeb5b5db2ba384f17514d84750f0
alotfi <damad36502@gmail.com>
2025-12-01 21:45:46 +0100

    first exercise finish

--- variable.js
+++ variable.js
@@ -1,3 +1,9 @@
-let monNom = "Abon"
+let bibli = 500
 
-console.log(monNom)
+bibli += 50
+bibli -= 10
+bibli += 5
+
+affichageTotalLivres = "Notre bibliothèque possède " + bibli + " livres"
+
+console.log(affichageTotalLivres)
