8933c4eb53bb63a5cfc11a73d069d9e2674a7f1d
alotfi <damad36502@gmail.com>
2025-12-02 12:43:03 +0100

    file structure add

---
+++ structure.js
@@ -0,0 +1,11 @@
+let ticket = {
+    nomFilm : "Batman",
+    prix : 14,
+    numeroSalle: 5
+}
+
+const nom = "Coco"
+
+let texteAffichage = "Bonjour " + nom + ", votre film" +ticket.nomFilm+ "est en salle " +ticket.numeroSalle+ "."
+
+console.log(texteAffichage)
