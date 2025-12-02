fb3a0509b0736fce5199548d753519209d5d6f8f
alotfi <damad36502@gmail.com>
2025-12-02 20:13:56 +0100

    Feature: allow the user to enter a word and verify if it matches the correct answer.

---
+++ script.js
@@ -0,0 +1,17 @@
+let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+
+let score = 0 
+
+let motUtilisateur = prompt("Rentrez le mot : " + listeMots[0])
+
+if (motUtilisateur === listeMots[0]) { score++ }
+
+motUtilisateur = prompt("Rentrez le mot : " + listeMots[1])
+
+if (motUtilisateur === listeMots[1]) { score++ }
+
+motUtilisateur = prompt("Rentrez le mot : " + listeMots[2])
+
+if (motUtilisateur === listeMots[2]) { score++ }
+
+console.log(score)
