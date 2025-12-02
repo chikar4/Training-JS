8caee64faca529f7d5e6293d797a8e38625a290e
alotfi <damad36502@gmail.com>
2025-12-02 13:52:49 +0100

    new branch add : Partie1

---
+++ tableaux.js
@@ -0,0 +1,15 @@
+let playlist = ["song1", "song2", "song3"]
+
+playlist.push("song4", "song5")
+
+let totalMorceaux = playlist.length
+
+console.log(totalMorceaux)
+console.log(playlist)
+
+playlist.pop()
+
+totalMorceaux = playlist.length
+
+console.log(totalMorceaux)
+console.log(playlist)
