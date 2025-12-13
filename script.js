3491d2f3d3da167870a90b5f64cb7a8d7c78f3ad
alotfi <damad36502@gmail.com>
2025-12-13 17:11:20 +0100

    feature function finish

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 let listeMots = ["Cachalot", "Pétunia", "Serviette"]
 let listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"]
 
-let choixUtilisateur = prompt("Voulez-vous une liste de mots ou une liste de phrase ?") 
-
-while (choixUtilisateur !== "phrases" && choixUtilisateur !== "mots") {
-    choixUtilisateur = prompt("Voulez-vous une liste de mots ou une liste de phrase ?")
+function choisirPhrasesOuMots() {
+    let choixUtilisateur = prompt("Voulez-vous une liste de mots ou une liste de phrase ?") 
+    while (choixUtilisateur !== "phrases" && choixUtilisateur !== "mots") {
+        choixUtilisateur = prompt("Voulez-vous une liste de mots ou une liste de phrase ?")
+    }
+    return choixUtilisateur
 }
 
-let score = 0
+function lancerBoucleDeJeu(listePropositions) {
+    let score = 0
+    for (let i = 0; i < 3; i++) {
+        let motUtilisateur = prompt("Rentrez le mot : " + listePropositions[i])
+        if (motUtilisateur === listePropositions[i]) {score++}
+    }
 
-if (choixUtilisateur === "mots") {
-    for (let i = 0; i < 3; i++) {
-        let motUtilisateur = prompt("Rentrez le mot : " + listeMots[i])
-        if (motUtilisateur === listeMots[i]) {score++}
-    }
-} else if (choixUtilisateur === "phrases") {
-    for (let i = 0; i < 3; i++) {
-        let motUtilisateur = prompt("Rentrez la phrase : " + listePhrases[i])
-        if (motUtilisateur === listePhrases[i]) {score++}
-    }
+    return score
+}
+
+function afficherResultat(score, nombreMots) {
+    console.log(score + " sur " + nombreMots)
 }
 
 
+function lancerJeu() {
+    let choix = choisirPhrasesOuMots()
+    let score = 0
+    let nombreMots = 0
 
-console.log(score)
+    if (choix === 'mots') {
+        score = lancerBoucleDeJeu(listeMots)
+        nombreMots = listeMots.length
+    } else {
+        score = lancerBoucleDeJeu(listePhrases)
+        nombreMots = listePhrases.length
+    }
+
+    afficherResultat(score, nombreMots)
+}
+
+lancerJeu()
